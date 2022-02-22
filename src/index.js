ca81b6e6432891bc5817f3deb3381200495222ab
Vitaliy Kozak <96038573+vitaliykez97@users.noreply.github.com>
2022-02-22 22:30:37 +0300

    end

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-module.exports = function towelSort (matrix) {
-  var result = matrix[0]
-  for (let i=1; i<matrix.length; i++){
-    if ( i%2 === 0){
-      result = result.concat(matrix[i])
-    } else {
-      result = result.concat(matrix[i].reverse())
-    }
-  }
+module.exports = function towelSort (matrix) { 
   
+  if (typeof(matrix) === 'object'){
+    var result = matrix[0]
+    if (matrix.length !== 0){
+        for (let i=1; i<matrix.length; i++){
+        if ( i%2 === 0){
+          result = result.concat(matrix[i])
+        } else {
+          result = result.concat(matrix[i].reverse())
+        } 
+      }
+    } else { result = [] }
+  } else { result = [] }
+
+
   console.log(result)
-  return [];
+  console.log(typeof(result))
+  return result;
 }
